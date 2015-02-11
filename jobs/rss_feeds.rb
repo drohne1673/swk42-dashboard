8fd83e6f7f4490e5aeb85bbe8ddee3a800e511d1
drohne1673 <drohne1673@gmx.net>
2015-02-11 10:03:12 +0100

    fixed querying

--- jobs/rss_feeds.rb
+++ jobs/rss_feeds.rb
@@ -1,6 +1,6 @@
 title = 'newsfeeds'
 
-SCHEDULER.every '5m' do
+SCHEDULER.every '30s' do
   http = Net::HTTP.new 'query.yahooapis.com'
   query = URI::encode "select title, description, source from rss where url='http://rss.news.yahoo.com/rss/topstories' limit 10&format=json"
   request = http.request Net::HTTP::Get.new("/v1/public/yql?q=#{query}")
--- jobs/stock_exchange.rb
+++ jobs/stock_exchange.rb
@@ -1,22 +1,51 @@
 title = 'stockexchange'
 
-SCHEDULER.every '5m' do
+company_symbol = 'UTDI.DE'
+
+SCHEDULER.every '30s' do
+
   http = Net::HTTP.new 'query.yahooapis.com'
-  query = URI::encode "select * from yahoo.finance.historicaldata where symbol = 'UTDI.DE' and startDate = '2014-09-11' and endDate = '2015-02-10'&format=json"
+  query = URI::encode "select * from yahoo.finance.historicaldata where symbol = '"+company_symbol+"' and startDate = '2014-09-11' and endDate = '2015-02-11'&env=store://datatables.org/alltableswithkeys&format=json"
   request = http.request Net::HTTP::Get.new("/v1/public/yql?q=#{query}")
   response = JSON.parse request.body
   results = response['query']['results']['quote']
 
   if results
-    items = Array.new
+    data_open = Array.new
+    data_high = Array.new
+    data_low = Array.new
+    data_close = Array.new
     results.each do |item|
 
-      puts 'item found with '+item['title']
-      items.push({:label => item['title'], :value => item['source']['content']})
+      date = Time.parse(item['Date']).to_i
 
+      data_open.push({:x => date, :y => item['Open']})
+      data_high.push({:x => date, :y => item['High']})
+      data_low.push({:x => date, :y => item['Low']})
+      data_close.push({:x => date, :y => item['Close']})
+
+      #puts "added line with #{date} and open "+ item['Open']
     end
+
+
+    series = [
+        {
+            :name => 'Open',
+            :data => data_open
+        }, {
+            :name => 'High',
+            :data => data_high
+        }, {
+            :name => 'Low',
+            :data => data_low
+        }, {
+            :name => 'Close',
+            :data => data_close
+        }
+    ]
+
     send_event title, {
-                        :items => items
+                        :series => series
                     }
   end
 end
